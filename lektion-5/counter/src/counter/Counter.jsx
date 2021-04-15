def9064db4c2d66906e88140153c13a48b56a8a1
marcusnilss0n <nilsson.marcus90@gmail.com>
2021-04-15 18:51:37 +0200

    Counter lektion 5 done

--- lektion-5/counter/src/counter/Counter.jsx
+++ lektion-5/counter/src/counter/Counter.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
-import Decreaser from "./Decreaser";
+import React, { useState } from 'react';
 import Increaser from "./Increaser";
+import Decreaser from "./Decreaser";
 
 const Counter = (props) => {
-  const [count, setCount] = useState(0);
+     const [count, setCount] = useState(0);
 
-  return (
+     return (
     <div>
-      <h1>{count}</h1>
-      <Decreaser count={count} amount={10} onDecrease={setCount} />
-      <Decreaser count={count} amount={5} onDecrease={setCount} />
-      <Decreaser count={count} amount={1} onDecrease={setCount} />
-      <Increaser count={count} amount={1} onIncrease={setCount} />
-      <Increaser count={count} amount={5} onIncrease={setCount} />
-      <Increaser count={count} amount={10} onIncrease={setCount} />
+        <h1>{count}</h1>
+        <Decreaser count={count} amount={10} onDecrease={setCount}/>
+        <Decreaser count={count} amount={5} onDecrease={setCount}/>
+        <Decreaser count={count} amount={1} onDecrease={setCount}/>
+        <Increaser count={count} amount={1} onIncrease={setCount}/>
+        <Increaser count={count} amount={5} onIncrease={setCount}/>
+        <Increaser count={count} amount={10} onIncrease={setCount}/>
     </div>
-  );
-};
+    );
+}
 
 export default Counter;
--- lektion-5/counter/src/counter/Decreaser.jsx
+++ lektion-5/counter/src/counter/Decreaser.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Decreaser = (props) => {
     const decrease = () => {
         let newValue = props.count - props.amount;
-        if(newValue < 0){
+        if(newValue <0){
             newValue = 0;
         }
         if(props.onDecrease){
